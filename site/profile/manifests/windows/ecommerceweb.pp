2ef1e19b4341a4375e9cb1c32452ffc03a8844ab
Nicolas Corrarello <ncorrare@gmail.com>
2016-09-20 10:41:01 +0100

    Added information for the EcommerceWeb and respective profiles

---
+++ site/profile/manifests/windows/ecommerceweb.pp
@@ -0,0 +1,47 @@
+class profile::windows::ecommerceweb (
+  $zippath,
+  $websitename,
+  $sourcerepo,
+  $destinationpath,
+  $zipname,
+  ){
+  require profile::windows::iis
+  $zipuri  = "${sourcerepo}/${zipname}"
+  $zipfile = "${zippath}\\${zipname}"
+  # Create tmp folder
+  dsc_file {'tmp folder':
+    dsc_ensure          => 'present',
+    dsc_type            => 'Directory',
+    dsc_destinationpath => $zippath,
+  } ->
+
+  # Download the site content
+  dsc_xremotefile {'Download WebContent Zip':
+    dsc_destinationpath => $zipfile,
+    dsc_uri             => $zipuri,
+  } ->
+
+  # Extract the website content
+  dsc_archive {'Unzip and Copy the WebContent':
+    dsc_ensure      => 'present',
+    dsc_path        => $zipfile,
+    dsc_destination => $destinationpath,
+  } ->
+
+  # Create a new Website
+  dsc_xwebsite {'BackeryWebSite':
+    dsc_ensure       => 'present',
+    dsc_name         => $websitename,
+    dsc_state        => 'Started',
+    dsc_physicalpath => $destinationpath,
+  }
+  # Add the firewall rule to allow HTTP Access
+  dsc_xfirewall { 'Allow HTTP':
+    dsc_name      => "FourthCoffee - Allow HTTP",
+    dsc_ensure    => 'present',
+    dsc_direction => 'Inbound',
+    dsc_localport => '80',
+    dsc_protocol  => 'TCP',
+    dsc_action    => 'Allow',
+  }
+}
---
+++ site/profile/manifests/windows/iis.pp
@@ -0,0 +1,16 @@
+class profile::windows::iss {
+  dsc_windowsfeature {'IIS':
+    dsc_ensure => 'present',
+    dsc_name   => 'Web-Server',
+  } ->
+  dsc_windowsfeature {'AspNet45':
+     dsc_ensure => 'present',
+     dsc_name   => 'Web-Asp-Net45',
+  } ->
+   dsc_xwebsite {'Stop DefaultSite':
+    dsc_ensure       => 'present',
+    dsc_name         => 'Default Web Site',
+    dsc_state        => 'Stopped',
+    dsc_physicalpath => 'C:\inetpub\wwwroot',
+  }
+}
