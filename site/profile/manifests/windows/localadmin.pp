98d1399a504f74bc98aed38750b034de7a65c6a8
Nicolas Corrarello <ncorrare@gmail.com>
2016-09-20 09:43:59 +0100

    Using Puppet's resource instead of DSC.

--- site/profile/manifests/windows/localadmin.pp
+++ site/profile/manifests/windows/localadmin.pp
@@ -2,18 +2,24 @@ class profile::windows::localadmin (
   $localadmin,
   $adminpassword,
   ) {
+  user { $localadmin:
+    ensure   => present,
+    comment  => 'Jaye Tyler',
+    password => $adminpassword,
+    groups   => ['BUILTIN/Administrators'],
+  }
   #Example Local Admin implementation, using DSC Types
 
-  dsc_user { $localadmin:
-    dsc_fullname             => 'Jaye Tyler',
-    dsc_description          => 'Local Administrator User',
-    dsc_ensure               => present,
-    dsc_username             => $localadmin,
-    dsc_password             => {
-      'user'     => $localadmin,
-      'password' => $adminpassword
-    },
-    dsc_passwordneverexpires => false,
-    dsc_disabled             => true,
-  }
+  #dsc_user { $localadmin:
+  #  dsc_fullname             => 'Jaye Tyler',
+  #  dsc_description          => 'Local Administrator User',
+  #  dsc_ensure               => present,
+  #  dsc_username             => $localadmin,
+  #  dsc_password             => {
+  #    'user'     => $localadmin,
+  #    'password' => $adminpassword
+  #  },
+  #  dsc_passwordneverexpires => false,
+  #  dsc_disabled             => true,
+  #}
 }
